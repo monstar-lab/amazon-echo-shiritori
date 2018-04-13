f231cb3cfce53aa118784e606918bc090629638e
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-13 18:31:21 +0900

    時間フォーマット修正

--- timeData/timeformat.go
+++ timeData/timeformat.go
@@ -6,9 +6,24 @@ import (
 )
 
 func GetNowTimeFormat() string {
-	time := time.Now()
+	// timeNow := time.Now()
+	// const layout = "2006-01-02 15:04:05"
+	// fmt.Println("timeNow: " + timeNow.Format(layout))
+	//日付フォーマット指定
+	const layout = "2006-01-02 15:04:05"
 
-	return fmt.Sprintf("%d/%d/%d %d:%d:%d\n", time.Year(), time.Month(), time.Day(), time.Hour(), time.Minute(), time.Second())
+	now := time.Now()
+	fmt.Println(now.Format(time.RFC3339))
+
+	nowUTC := now.UTC()
+	fmt.Println(nowUTC.Format(time.RFC3339))
+
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+
+	nowJST := nowUTC.In(jst)
+	fmt.Println(nowJST.Format(layout))
+	return nowJST.Format(layout)
+	//return timeNow.Format(layout)
 }
 
 //中断処理について、現在時刻と比べ、差が一番小さいものをだす
