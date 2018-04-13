53534c779a993a62a88b3ee770a19076ef598966
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-13 17:08:11 +0900

    関数まとめファイル

---
+++ function/func.go
@@ -0,0 +1,51 @@
+package function
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"../dataStructure"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+func test() {
+
+}
+
+//ユーザ返答単語を返す
+func ResWord(output *dynamodb.ScanOutput, keyword string) string {
+	// DBから取得したデータのJSONの形を変換
+	words := make([]*dataStructure.WordDB, 0)
+	unMarshaListOfMapErr := dynamodbattribute.UnmarshalListOfMaps(output.Items, &words)
+	if unMarshaListOfMapErr != nil {
+		panic(fmt.Sprintf("failed to unmarshal Dynamodb Scan Items, %v", unMarshaListOfMapErr))
+	}
+
+	bytes, _ := json.Marshal(words)
+
+	//変換されたデータ形をパースし、取得
+	var data []dataStructure.Words
+	unMarshaErr := json.Unmarshal(bytes, &data)
+	if unMarshaErr != nil {
+		fmt.Println("error:", unMarshaErr)
+	}
+
+	for _, word := range data {
+		fmt.Printf("word_id: %v, word: %v\n", word.WordID, word.Word)
+		isWord := CheckWord(word.Word, keyword)
+		if isWord == true {
+			return word.Word
+		}
+	}
+	return ""
+}
+
+//文字列の先頭部分は末尾文字と一致しているかどうか
+func CheckWord(value string, keyword string) bool {
+
+	//fmt.Println(strings.HasPrefix("ナツ", "ツナミ"))
+	fmt.Println(strings.HasPrefix(value, keyword))
+	return strings.HasPrefix(value, keyword)
+}
