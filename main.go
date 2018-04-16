f8b1929a14f03b29a0fa800395e70ef085fbaa55
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-16 16:06:45 +0900

    末尾の違い認識処理と末尾がんで終わると負け知らせ処理

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 
 	"./alexa"
 	"./db"
-	"./timeData"
+	"./function"
 	"github.com/aws/aws-lambda-go/lambda"
 )
-
-const GAME_START_MESSAGE = `しりとりスキルへようこそ。「りんご」`
-
-const RES_SHIRITORI_SLOT = "shiritoriword"
 
 var (
 	ErrInvalidIntent = errors.New("Invalid intent")
 )
+
+//最後に返答した単語を格納
+var lastWord = ""
 
 // OnLaunch is function-type
 func OnLaunch(launchRequest alexa.RequestDetail) (alexa.Response, error) {
@@ -27,6 +26,8 @@ func OnLaunch(launchRequest alexa.RequestDetail) (alexa.Response, error) {
 // GetWelcomeResponse is function-type
 func GetWelcomeResponse() alexa.Response {
 
+	//ユーザー最後返答する単語を格納する変数を初期値に戻す
+	lastWord = ""
 	cardTitle := " しりとり"
 	speechOutput := GAME_START_MESSAGE
 	repromptText := GAME_START_MESSAGE
@@ -45,39 +46,49 @@ func OnIntent(intentRequest alexa.RequestDetail) (alexa.Response, error) {
 		return getShiritoriWord(intentRequest.Intent.Slots["shiritoriword"].Value)
 	}
 	return GetWelcomeResponse(), nil
-	//  else
-	//  return GetWelcomeResponse()
-	// cardTitle := " しりとり"
-	// speechOutput := GAME_START_MESSAGE
-	// repromptText := GAME_START_MESSAGE
-	// shouldEndSession := true
-	//return alexa.BuildResponse(alexa.BuildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession)), nil
-	//	return alexa.Response{}, ErrInvalidIntent
-	// return alexa.BuildResponse(alexa.BuildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession)), nil
 }
 
 func getShiritoriWord(value string) (alexa.Response, error) {
+	//ユーザー返答した単語を取得
 	value = strings.TrimSpace(value)
+
 	//文字列を分割して、末尾文字を取得
 	arr := strings.Split(value, "")
 	lastCharacter := arr[len(arr)-1]
-	//データベースに登録
-	db.PutWord(value, 1)
-	log.Print(lastCharacter)
-	//末尾文字を取得後データベースに参照、単語を取得して
-	res := db.GetWordData(lastCharacter)
 
-	//ユーザーに単語をお知らせ
+	//始まり文字を取得
+	firstCharacter := arr[0]
 
-	if res == "" {
-		res = "負けました。"
+	//ユーザに返すレスポンス初期値を設定
+	res := ""
+
+	//末尾チェック
+	if function.CheckN(lastCharacter) {
+		//末尾が「ん」
+		res = LOSS_N_MESSAGE
+	} else if function.CheckEndOfTheWordIsWrong(firstCharacter, lastWord) == true {
+		//語尾が違う
+		res = WRONG_END_WORD
+	} else {
+		//データベースに登録
+		db.PutWord(value, 1)
+		log.Print(lastCharacter)
+		log.Print(firstCharacter)
+		//末尾文字を取得後データベースに参照、単語を取得して
+		res = db.GetWordData(lastCharacter)
+		//最後に返答した単語を値保持
+		lastWord = res
+		//ユーザーに単語をお知らせ
+		if res == "" {
+			res = "負けました。"
+		}
+
+		log.Print(value + ": check")
+
 	}
-
-	log.Print(value + ": check")
-
 	cardTitle := " しりとりインテント"
 	speechOutput := res
-	repromptText := res + timeData.GetNowTimeFormat()
+	repromptText := res
 	shouldEndSession := true
 
 	return alexa.BuildResponse(alexa.BuildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession)), nil
