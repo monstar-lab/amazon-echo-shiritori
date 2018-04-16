f0cadafcb1233d5fabc02d9647985c3fc7251247
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-16 16:07:06 +0900

    ユーザーに返答するメッセージを格納する

--- responseData.go
+++ responseData.go
@@ -1,3 +1,5 @@
 package main
 
-const GAME_START = "ようこそ、しりとりゲーム、ではしりとり開始します。えいご"
+const GAME_START_MESSAGE = "しりとりスキルへようこそ、ではしりとりゲームを開始します。"
+const LOSS_N_MESSAGE = "語尾がんで終わったので、私の勝ちです。"
+const WRONG_END_WORD = "語尾が違います、新しい単語をあげてください。"
