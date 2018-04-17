54af299b0ed06767c26bc7d7d247887292ddc014
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-17 17:59:52 +0900

    現在日時取得、指定されたフォーマットにしたがって値を返す

--- timeData/timeformat.go
+++ timeData/timeformat.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-func GetNowTimeFormat() string {
-	const layout = "2006-01-02 15:04:05"
+func GetNowTimeFormat(layout string) string {
+	//const layout = "2006-01-02 15:04:05"
 
 	now := time.Now()
 	fmt.Println(now.Format(time.RFC3339))
@@ -23,3 +23,8 @@ func GetNowTimeFormat() string {
 }
 
 //中断処理について、現在時刻と比べ、差が一番小さいものをだす
+
+//DBのIDとして現在日時のフォーマットを変換
+// func GetDBIDFormat() string {
+
+// }
