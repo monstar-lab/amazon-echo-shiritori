f552c2c859ba3bca9abdda4795b0ddd7ceaf4e15
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-17 11:38:51 +0900

    漢字をふりがなに変換するAPI のアクセスIDとURLの追加

--- responseData.go
+++ responseData.go
@@ -5,3 +5,5 @@ const LOSS_N_MESSAGE = "語尾がんで終わったので、私の勝ちです�
 const WRONG_END_WORD = "語尾が違います、新しい単語をあげてください。"
 const LOSS_GAME = "負けました。"
 const ANSWER_MSG = "ですね、じゃ"
+const API_ACCESS_ID = "dj00aiZpPVJ3M0dHRjdLRDM2biZzPWNvbnN1bWVyc2VjcmV0Jng9MTU-"
+const URL = "http://jlp.yahooapis.jp/FuriganaService/V1/furigana"
