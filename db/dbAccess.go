429f65c97da2ffddf98680a989ea0a32fed79bb6
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-18 13:06:12 +0900

    ゲーム情報と単語登録関数追加

--- db/dbAccess.go
+++ db/dbAccess.go
@@ -48,19 +48,7 @@ func GetWordData(keyword string) string {
 
 }
 
-// get Max Id
-// func GetMaxID() {
-// 	cred := credentials.NewStaticCredentials(ACCESS_KET_ID, SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
-
-// 	db := dynamo.New(session.New(), &aws.Config{
-// 		Credentials: cred,
-// 		Region:      aws.String("ap-northeast-1"), // "ap-northeast-1"等
-// 	})
-
-// }
-
-//単語をデータベースに登録
-func PutWord(word string, flag int) {
+func PutGameInfo(flag int) string {
 	cred := credentials.NewStaticCredentials(ACCESS_KET_ID, SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
 
 	db := dynamo.New(session.New(), &aws.Config{
@@ -70,13 +58,74 @@ func PutWord(word string, flag int) {
 
 	table := db.Table("history")
 
-	history := dataStructure.History{HistoryID: timeData.GetNowTimeFormat(constant.DB_ID_FORMAT), Time: timeData.GetNowTimeFormat(constant.DB_INSERT_TIME_FORMAT), Flag: flag}
+	//history idを作成
+	historyID := timeData.GetNowTimeFormat(constant.DB_ID_FORMAT)
+
+	history := dataStructure.History{HistoryID: historyID, Time: timeData.GetNowTimeFormat(constant.DB_INSERT_TIME_FORMAT), Flag: flag}
 	//u := User{User_ID: "lambda test"}
 	fmt.Println(history)
 
 	if err := table.Put(history).Run(); err != nil {
 		fmt.Println("err")
 		panic(err.Error())
+	}
+	return historyID
+}
+
+//単語をデータベースに登録
+func PutWord(word string, historyID string, answerer string, flag int) {
+	log.Println("db -> " + word)
+	cred := credentials.NewStaticCredentials(ACCESS_KET_ID, SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
+
+	db := dynamo.New(session.New(), &aws.Config{
+		Credentials: cred,
+		Region:      aws.String("ap-northeast-1"), // "ap-northeast-1"等
+	})
+
+	table := db.Table("history_detail")
+
+	historyDetail := dataStructure.HistoryDetail{HistoryDetailID: timeData.GetNowTimeFormat(constant.DB_ID_FORMAT), HistoryID: historyID, Time: timeData.GetNowTimeFormat(constant.DB_INSERT_TIME_FORMAT), Answerer: answerer, Answer: word, Flag: flag}
+	fmt.Println(historyDetail)
+
+	err := table.Put(historyDetail).Run()
+	if err != nil {
+		fmt.Println("err")
+		panic(err.Error())
+	}
+}
+
+//historyテーブルのフラグ変更
+func UpdateItem(flag int, historyID string) {
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	db := dynamodb.New(sess, &aws.Config{
+		Credentials: credentials.NewStaticCredentials(ACCESS_KET_ID, SECRET_ACCESS_KEY, ""),
+		Region:      aws.String("ap-northeast-1"), // "ap-northeast-1"等
+	})
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":flag": {
+				N: aws.String(fmt.Sprintf("%v", flag)),
+			},
+		},
+		TableName: aws.String("history"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"history_id": {
+				S: aws.String(historyID),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set flag = :flag"),
+	}
+
+	_, err = db.UpdateItem(input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 
 }
