825bd5167e7a0e67e115443378f79fd78a2bbb6a
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-04-19 10:32:59 +0900

    DB Reginを定義

--- constant/constantData.go
+++ constant/constantData.go
@@ -1,6 +1,6 @@
 package constant
 
-const GAME_START_MESSAGE = "しりとりスキルへようこそ、ではしりとりゲームを開始します。"
+const GAME_START_MESSAGE = "しりとりスキルへようこそ、ではしりとりゲームを開始します。単語を上げますね。"
 const LOSS_N_MESSAGE = "語尾がんで終わったので、私の勝ちです。"
 const WRONG_END_WORD = "語尾が違います、新しい単語をあげてください。"
 const LOSS_GAME = "負けました。"
@@ -11,6 +11,7 @@ const URL = "http://jlp.yahooapis.jp/FuriganaService/V1/furigana"
 //DBに使うデータ定義
 const ACCESS_KEY_ID = "AKIAJZOEOAYI7AKKQ7AQ"
 const SECRET_ACCESS_KEY = "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"
+const REGION = "ap-northeast-1"
 
 const DB_INSERT_TIME_FORMAT = "2006-01-02 15:04:05"
 const DB_ID_FORMAT = "20060102150405"
