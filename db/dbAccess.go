8cfff34b1c8a9e2d18f0a42c70fdb05d10f1c801
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-05-01 17:35:31 +0900

    DB構造を変更して、DBにアクセス必要な関数を変更および追加

--- db/dbAccess.go
+++ db/dbAccess.go
@@ -1,23 +1,23 @@
 package db
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"strings"
 
 	"../constant"
 	"../dataStructure"
-	"../function"
 	"../timeData"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/guregu/dynamo"
 )
 
-//DBから単語を取得
-func GetWordData(keyword string) string {
-	log.Print(keyword)
+//DBから単語を取得 何から始まる単語を word_v2
+func GetDBWordList(keyword string) []string {
 	cred := credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
 
 	db := dynamodb.New(session.New(), &aws.Config{
@@ -25,33 +25,117 @@ func GetWordData(keyword string) string {
 		Region:      aws.String(constant.REGION), // constant.REGION等
 	})
 
-	getParams := &dynamodb.ScanInput{
-		TableName: aws.String("word"),
+	getParams := &dynamodb.QueryInput{
+		TableName: aws.String(constant.DB_WORD),
 
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
 			":wordID": {
-				N: aws.String("1"),
+				S: aws.String(keyword),
 			},
 			":word": { // :を付けるのがセオリーのようです
 				S: aws.String(keyword),
 			},
 		},
-		// word_id が 1以上と「keyword」から始まりの単語
-		FilterExpression: aws.String("word_id >= :wordID and begins_with(word, :word)"),
+		KeyConditionExpression: aws.String("word_id = :wordID and begins_with(word, :word)"),
 	}
 
-	getItem, getErr := db.Scan(getParams)
+	getItem, getErr := db.Query(getParams)
 
 	if getErr != nil {
 		panic(getErr)
 	}
-	fmt.Println(getItem)
-
-	return function.ResWord(getItem, keyword)
+	return makeWordList(getItem, keyword)
 
 }
 
-func PutGameInfo(flag int) string {
+//単語全て取得きたら　単語だけのリストを作成
+func makeWordList(output *dynamodb.QueryOutput, keyword string) []string {
+	res := []string{}
+	words := make([]*dataStructure.WordDB, 0)
+	unMarshaListOfMapErr := dynamodbattribute.UnmarshalListOfMaps(output.Items, &words)
+	if unMarshaListOfMapErr != nil {
+		panic(fmt.Sprintf("failed to unmarshal Dynamodb Scan Items, %v", unMarshaListOfMapErr))
+	}
+
+	bytes, _ := json.Marshal(words)
+
+	//変換されたデータ形をパースし、取得
+	var data []*dataStructure.Words
+	unMarshaErr := json.Unmarshal(bytes, &data)
+	if unMarshaErr != nil {
+		fmt.Println("error:", unMarshaErr)
+	}
+
+	for _, word := range data {
+		fmt.Printf("word_id: %v, word: %v\n", word.WordID, word.Word)
+		res = append(res, word.Word)
+	}
+	return res
+}
+
+//history_detail_v2から一回のゲームで回答した単語全て
+func GetHistoryWord(historyDetailID string) string {
+	cred := credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
+
+	db := dynamodb.New(session.New(), &aws.Config{
+		Credentials: cred,
+		Region:      aws.String(constant.REGION), // constant.REGION等
+	})
+
+	getParams := &dynamodb.GetItemInput{
+		TableName: aws.String("history_detail_v2"),
+
+		Key: map[string]*dynamodb.AttributeValue{
+			"history_id": {
+				S: aws.String(historyDetailID),
+			},
+		},
+		AttributesToGet: []*string{
+			aws.String("answer"), // 欲しいデータの名前
+		},
+	}
+
+	getItem, getErr := db.GetItem(getParams)
+
+	if getErr != nil {
+		panic(getErr)
+	}
+	return *getItem.Item["answer"].S
+}
+
+//DBから取得してきた単語と単語リストを比較し、重複してない単語最初の一件を返す
+func ResNotUesWord(userWord string, wordList []string) string {
+	//DBから取得きた単語を配列に変換
+	oneRes := strings.Split(userWord, ";")
+	for i := 0; i < len(oneRes)-1; i++ {
+		oneWord := strings.Split(oneRes[i], ",")
+
+		//重複した単語を削除
+		for j := 0; j < len(oneWord)-1; j++ {
+			wordList = delete_strings(wordList, oneWord[j])
+		}
+	}
+	if len(wordList) != 0 {
+		return wordList[0]
+	}
+	return ""
+}
+
+//重複したものを削除
+func delete_strings(slice []string, s string) []string {
+	ret := make([]string, len(slice))
+	i := 0
+	for _, x := range slice {
+		if s != x {
+			ret[i] = x
+			i++
+		}
+	}
+	return ret[:i]
+}
+
+//新規ゲーム登録
+func PutHistoryDetailData(answer string, flag int) string {
 	cred := credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
 
 	db := dynamo.New(session.New(), &aws.Config{
@@ -59,14 +143,11 @@ func PutGameInfo(flag int) string {
 		Region:      aws.String(constant.REGION), // constant.REGION等
 	})
 
-	table := db.Table("history")
-
+	table := db.Table(constant.DB_USE_WORD_HISTORY)
 	//history idを作成
 	historyID := timeData.GetNowTimeFormat(constant.DB_ID_FORMAT)
 
-	history := dataStructure.History{HistoryID: historyID, Time: timeData.GetNowTimeFormat(constant.DB_INSERT_TIME_FORMAT), Flag: flag}
-	//u := User{User_ID: "lambda test"}
-	fmt.Println(history)
+	history := dataStructure.HistoryDetail{HistoryID: historyID, Answer: answer, Flag: flag}
 
 	if err := table.Put(history).Run(); err != nil {
 		fmt.Println("err")
@@ -75,30 +156,8 @@ func PutGameInfo(flag int) string {
 	return historyID
 }
 
-//単語をデータベースに登録
-func PutWord(word string, historyID string, answerer string, flag int) {
-	log.Println("db -> " + word)
-	cred := credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, "") // 最後の引数は[セッショントークン]
-
-	db := dynamo.New(session.New(), &aws.Config{
-		Credentials: cred,
-		Region:      aws.String(constant.REGION), // constant.REGION等
-	})
-
-	table := db.Table("history_detail")
-
-	historyDetail := dataStructure.HistoryDetail{HistoryDetailID: timeData.GetNowTimeFormat(constant.DB_ID_FORMAT), HistoryID: historyID, Time: timeData.GetNowTimeFormat(constant.DB_INSERT_TIME_FORMAT), Answerer: answerer, Answer: word, Flag: flag}
-	fmt.Println(historyDetail)
-
-	err := table.Put(historyDetail).Run()
-	if err != nil {
-		fmt.Println("err")
-		panic(err.Error())
-	}
-}
-
-//historyテーブルのフラグ変更
-func UpdateItem(flag int, historyID string) {
+//history_detail_v2 テーブルのフラグ変更
+func UpdateHistoryFlag(flag int, historyID string) {
 	sess, err := session.NewSession()
 	if err != nil {
 		panic(err)
@@ -114,7 +173,7 @@ func UpdateItem(flag int, historyID string) {
 				N: aws.String(fmt.Sprintf("%v", flag)),
 			},
 		},
-		TableName: aws.String("history"),
+		TableName: aws.String(constant.DB_USE_WORD_HISTORY),
 		Key: map[string]*dynamodb.AttributeValue{
 			"history_id": {
 				S: aws.String(historyID),
@@ -130,7 +189,39 @@ func UpdateItem(flag int, historyID string) {
 		fmt.Println(err.Error())
 		return
 	}
+}
 
+//history_detail_v2 のanswerを変更
+func UpdateHistoryDetailAnswer(answer string, historyID string) {
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+	db := dynamodb.New(sess, &aws.Config{
+		Credentials: credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, ""),
+		Region:      aws.String(constant.REGION), // constant.REGION等
+	})
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":answer": {
+				S: aws.String(answer),
+			},
+		},
+		TableName: aws.String(constant.DB_USE_WORD_HISTORY),
+		Key: map[string]*dynamodb.AttributeValue{
+			"history_id": {
+				S: aws.String(historyID),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set answer = :answer"),
+	}
+	_, err = db.UpdateItem(input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
 
 func GetGameStartWord(wordID int) string {
@@ -170,41 +261,9 @@ func SearchWordCount(historyID string, word string) int {
 		// word_id が 1以上と「keyword」から始まりの単語
 		FilterExpression: aws.String("history_id = :historyID and answer = :answer"),
 	}
-
 	getItem, getErr := db.Scan(getParams)
-
 	if getErr != nil {
 		panic(getErr)
 	}
-
 	return len(getItem.Items)
 }
-
-/* データベース上に登録されたMax IDを取得
-* Scanは1MBのデータしか取得できない
-* 解決する必要がる
-* LastEvaluatedKeyを利用
- */
-// func GetMaxID() {
-// 	sess, err := session.NewSession()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	db := dynamodb.New(sess, &aws.Config{
-// 		Credentials: credentials.NewStaticCredentials(constant.ACCESS_KEY_ID, constant.SECRET_ACCESS_KEY, ""),
-// 		Region:      aws.String(constant.REGION), // constant.REGION等
-// 	})
-
-// 	getParams := &dynamodb.ScanInput{
-// 		TableName: aws.String("word"),
-// 		Select:    aws.String("COUNT"),
-// 	}
-// 	getItem, getErr := db.Scan(getParams)
-// 	if getErr != nil {
-// 		fmt.Println(getErr)
-// 		return
-// 	}
-
-// 	fmt.Println(string(getItem.Count))
-// }
