0725a286a20508828089fe7a48b12dda9fdaf845
DaiYingFei <daiyingfei@emueru-00066noMacBook-puro.local>
2018-05-08 15:21:15 +0900

    ゲームメッセージ追加

--- constant/constantData.go
+++ constant/constantData.go
@@ -1,7 +1,9 @@
 package constant
 
 const GAME_START_MESSAGE = "しりとりスキルへようこそ、ではゲーム開始。"
-const GAME_STOP_MEESAGE = "しりとりゲームスキルを終了します。"
+const GAME_STOP_MEESAGE = "しりとりゲームスキルを中止します。"
+const GAME_CANCEL_MEESAGE = "しりとりゲームスキルを終了します。"
+const GAME_RESUME_MEESAGE = "ゲームを再開します。"
 const LOSS_N_MESSAGE = "語尾がんで終わったので、私の勝ちです。"
 const WRONG_END_WORD = "語尾が違います、新しい単語をあげてください。"
 const IS_EXIST_WORD = "単語すでに使われていたので、新しい単語をあげてください"
@@ -12,7 +14,7 @@ const URL = "http://jlp.yahooapis.jp/FuriganaService/V1/furigana"
 
 //DBに使うデータ定義
 const DB_WORD = "word_v2"
-const DB_USE_WORD_HISTORY = "history_detail_v2"
+const DB_USE_WORD_HISTORY = "history_detail"
 
 //DBにアクセスするさいに必要なキーなど
 const ACCESS_KEY_ID = "AKIAJZOEOAYI7AKKQ7AQ"
@@ -22,13 +24,16 @@ const REGION = "ap-northeast-1"
 const DB_INSERT_TIME_FORMAT = "2006-01-02 15:04:05"
 const DB_ID_FORMAT = "20060102150405"
 
-const FIRST_GAME_FLAG = 0
-const AFTER_GAME_FLAG = 1
-const STOP_GAME_FLAG = 2
-const END_GAME_FLAG = 3
+// const FIRST_GAME_FLAG = 0
+// const AFTER_GAME_FLAG = 1
+// const STOP_GAME_FLAG = 2
+
+//const END_GAME_FLAG = 3
 
 const ANSWERER_ECHO = "echo"
 const ANSWERER_USER = "user"
 
 const LAST_ANSWERER = 1
 const NOT_LAST_ANSWERER = -1
+
+const NOT_FOUND_VALUE = "聞き取れませんでした。もう一度お願いします。"
